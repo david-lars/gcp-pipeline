cc26d03b5c597e9228ad7e85f3bf4dcf3a1aa3bc
David Lars <davidlars@davids-mbp-3.home>
2024-04-23 09:18:32 +0200

    initial code

--- main.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "my-bucket" {
-  name          = "terraform-github-bucket"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-}
