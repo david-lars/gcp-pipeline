71578ba0e6cb986d4b91fcbaad862052265eb15a
David Lars <davidlars@davids-mbp-3.home>
2024-04-23 10:47:38 +0200

    main.tf update

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name          = "terraform-github-bucket"
+  project       = "cloudrun-github-dev"
   location      = "US"
   force_destroy = true
 
